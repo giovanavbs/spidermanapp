75624b67d300e2708456947f8e6aca5db14c0e03
giovanavbs <giovana.santos261@etec.sp.gov.br>
2023-09-01 12:16:45 -0300

    adições activity

---
+++ app/src/main/java/com/example/smapp/menub1.java
@@ -0,0 +1,32 @@
+package com.example.smapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class menub1 extends AppCompatActivity {
+
+    private Button btnmn1;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menub1);
+
+        btnmn1 = (Button)findViewById(R.id.btnmn1);
+
+        btnmn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), brinquedo1.class);
+
+                //Intent intent = new Intent( this, brinquedo1.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
