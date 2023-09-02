8651fd90c253b50227c32b04a9389bfe17ede6cf
giovanavbs <giovana.santos261@etec.sp.gov.br>
2023-09-02 17:57:50 -0300

    botão

--- app/src/main/java/com/example/smapp/menub3.java
+++ app/src/main/java/com/example/smapp/menub3.java
@@ -2,13 +2,33 @@ package com.example.smapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class menub3 extends AppCompatActivity {
 
+    private Button btnmn3;
+
+    private Button btnmv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menub3);
+
+        btnmn3 = (Button)findViewById(R.id.btnmn3);
+
+        btnmv3 = (Button)findViewById(R.id.btnmv3);
+
+        btnmn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), brinquedo3.class);
+
+                //Intent intent = new Intent( this, brinquedo1.class);
+                startActivity(intent);
+            }
+        });
     }
 }
